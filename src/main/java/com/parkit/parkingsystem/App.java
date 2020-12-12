baeb15240b0cee78b5e64b9e25a11b114cda040f
claudiuvilau <vilau_claudiu@yahoo.com>
2020-12-12 01:53:47 +0100

    fin

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -9,7 +9,6 @@ import com.parkit.parkingsystem.service.InteractiveShell;
  * @author Claudiu
  *
  *         this is the begin of the program
- *
  */
 public class App {
 	private static final Logger logger = LogManager.getLogger("App");
